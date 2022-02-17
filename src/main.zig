f8c1dd8879e5a97242fef1e49d5a80d274cd4a34
Inhof Dávid <yrwqid@gmail.com>
2022-02-17 20:22:32 +0100

    :heart:

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,32 @@
 const std = @import("std");
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+const max_size = 1 * 1024 * 1024;
+
+fn usage() void {
+    std.os.exit(1);
 }
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+pub fn main() !void {
+    const alloc = std.heap.page_allocator;
+
+    var lines = std.ArrayList([]u8).init(alloc);
+
+    const args = try std.process.argsAlloc(alloc);
+    defer alloc.free(args);
+
+
+    if (args.len <= 1) {
+        usage();
+    }
+
+
+    const file = try std.fs.cwd().openFile(args[1], .{});
+    defer file.close();
+    while (try file.reader().readUntilDelimiterOrEofAlloc(alloc, '\n', max_size)) |line| {
+        try lines.append(line);
+    }
+
+    for (lines.items) |item|
+        std.debug.print("{s}\n", .{item});
+
 }
